8a3a93c6e2e9813d158db5fbddbdd852bb227897
Andrew Byala <abyala@yahoo.com>
2020-10-10 15:45:28 -0500

    Removed initial flatten

--- src/advent_2016_clojure/day3.clj
+++ src/advent_2016_clojure/day3.clj
@@ -1,22 +1,27 @@
 (ns advent-2016-clojure.day3
   (:require [clojure.string :as str]))
 
-(defn parse-to-int-seq [input]
+(defn parse-line [line]
   (map #(Integer/parseInt %)
-       (-> input str/trim (str/split #"\W+"))))
+       (-> line str/trim (str/split #" +"))))
+
+(defn parse-input [input]
+  (->> input
+       str/split-lines
+       (map parse-line)))
 
 (defn is-triangle? [sides]
   (let [[small2 largest] (map #(reduce + %)
                               (partition-all 2 (sort sides)))]
     (> small2 largest)))
 
-(defn by-rows [int-seq] (partition 3 int-seq))
+(def by-rows identity)
 (defn by-cols [int-seq]
-  (->> int-seq (partition 3) (apply interleave) (partition 3)))
+  (->> int-seq (apply interleave) (partition 3)))
 
 (defn count-triangles [grouping-fun input]
   (->> input
-       parse-to-int-seq
+       parse-input
        grouping-fun
        (filter is-triangle?)
        count))
