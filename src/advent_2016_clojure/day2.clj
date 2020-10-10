c5308fcbfc81a9c9b47306adcaec3747ce5e3064
Andrew Byala <abyala@yahoo.com>
2020-10-10 11:48:18 -0500

    Day 2 part 2

--- src/advent_2016_clojure/day2.clj
+++ src/advent_2016_clojure/day2.clj
@@ -2,23 +2,50 @@
   (:require [clojure.string :as str]
             [advent-2016-clojure.utils :refer [abs]]))
 
-(defn move [button direction]
-  (or
-    (case direction
-      \U (when (> button 3) (- button 3))
-      \D (when (< button 7) (+ button 3))
-      \L (when (not= (mod button 3) 1) (dec button))
-      \R (when (not= (mod button 3) 0) (inc button)))
-    button))
+(def normal-keypad
+  {1 {\R 2 \D 4}
+   2 {\L 1 \R 3 \D 5}
+   3 {\L 2 \D 6}
+   4 {\R 5 \U 1 \D 7}
+   5 {\L 4 \R 6 \U 2 \D 8}
+   6 {\L 5 \U 3 \D 9}
+   7 {\R 8 \U 4}
+   8 {\L 7 \R 9 \U 5}
+   9 {\L 8 \U 6}})
 
-(defn next-code [start instructions]
-  (reduce #(move %1 %2) start (vec instructions)))
+(def diamond-keypad
+  {1  {\D 3}
+   2  {\R 3 \D 6}
+   3  {\U 1 \L 2 \R 4 \D 7}
+   4  {\L 3 \D 8}
+   5  {\R 6}
+   6  {\L 5 \R 7 \U 2 \D \A}
+   7  {\L 6 \R 8 \U 3 \D \B}
+   8  {\L 7 \R 9 \U 4 \D \C}
+   9  {\L 8}
+   \A {\R \B \U 6}
+   \B {\L \A \R \C \U 7 \D \D}
+   \C {\L \B \U 8}
+   \D {\U \B}})
 
-(defn part1 [input]
+(defn move [keypad button direction]
+  (or ((keypad button) direction)
+      button))
+
+(defn next-button [keypad start instructions]
+  (reduce #(move keypad %1 %2) start (vec instructions)))
+
+(defn calculate-code [keypad input]
   (let [lines (str/split-lines input)
         final-state (reduce (fn [{start :start codes :codes} line]
-                              (let [next (next-code start line)]
+                              (let [next (next-button keypad start line)]
                                 {:start next :codes (str codes next)}))
                             {:start 5 :codes ""}
                             lines)]
     (:codes final-state)))
+
+(defn part1 [input]
+  (calculate-code normal-keypad input))
+
+(defn part2 [input]
+  (calculate-code diamond-keypad input))
--- test/advent_2016_clojure/day2_test.clj
+++ test/advent_2016_clojure/day2_test.clj
@@ -2,24 +2,42 @@
   (:require [clojure.test :refer :all]
             [advent-2016-clojure.day2 :refer :all]))
 
+(def TEST_INPUT "ULL\nRRDDD\nLURDL\nUUUUD")
 (def PUZZLE_INPUT (slurp "test\\advent_2016_clojure\\day2-data.txt"))
-
 
 (deftest move-test
   (let [directions '(\U \D \L \R)]
-    (is (= '(1 4 1 2)
-          (map #(move 1 %) directions)))
-    (is (= '(2 8 4 6)
-           (map #(move 5 %) directions)))
-    (is (= '(6 9 8 9)
-           (map #(move 9 %) directions)))))
+    (testing "normal keypad"
+      (is (= '(1 4 1 2)
+             (map #(move normal-keypad 1 %) directions)))
+      (is (= '(2 8 4 6)
+             (map #(move normal-keypad 5 %) directions)))
+      (is (= '(6 9 8 9)
+             (map #(move normal-keypad 9 %) directions))))
+    (testing "diamond keypad"
+      (is (= '(2 6 2 3)
+             (map #(move diamond-keypad 2 %) directions)))
+      (is (= '(3 \B 6 8)
+             (map #(move diamond-keypad 7 %) directions)))
+      (is (= '(8 \C \B \C)
+             (map #(move diamond-keypad \C %) directions))))))
 
-(deftest next-code-test
-  (is (= 1 (next-code 5 "ULL")))
-  (is (= 9 (next-code 1 "RRDDD")))
-  (is (= 8 (next-code 9 "LURDL")))
-  (is (= 5 (next-code 8 "UUUUD"))))
+(deftest next-button-test
+  (testing "normal keypad"
+    (is (= 1 (next-button normal-keypad 5 "ULL")))
+    (is (= 9 (next-button normal-keypad 1 "RRDDD")))
+    (is (= 8 (next-button normal-keypad 9 "LURDL")))
+    (is (= 5 (next-button normal-keypad 8 "UUUUD"))))
+  (testing "diamond keypad"
+    (is (= 5 (next-button diamond-keypad 5 "ULL")))
+    (is (= \D (next-button diamond-keypad 5 "RRDDD")))
+    (is (= \B (next-button diamond-keypad \D "LURDL")))
+    (is (= 3 (next-button diamond-keypad \B "UUUUD")))))
 
 (deftest part1-test
-  (is (= "1985" (part1 "ULL\nRRDDD\nLURDL\nUUUUD")))
+  (is (= "1985" (part1 TEST_INPUT)))
   (is (= "74921" (part1 PUZZLE_INPUT))))
+
+(deftest part2-test
+  (is (= "5DB3" (part2 TEST_INPUT)))
+  (is (= "A6B35" (part2 PUZZLE_INPUT))))
