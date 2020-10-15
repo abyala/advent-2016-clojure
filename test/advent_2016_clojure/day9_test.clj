1dd067f0878068754d5082ae8fd025cdf30bb258
Andrew Byala <abyala@yahoo.com>
2020-10-14 20:24:06 -0500

    Forgot to run the final test case!

--- test/advent_2016_clojure/day9_test.clj
+++ test/advent_2016_clojure/day9_test.clj
@@ -21,4 +21,6 @@
     (is (= 9 (part2 "(3x3)XYZ")))
     (is (= 20 (part2 "X(8x2)(3x3)ABCY")))
     (is (= 241920 (part2 "(27x12)(20x12)(13x14)(7x10)(1x12)A")))
-    (is (= 445 (part2 "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN")))))
+    (is (= 445 (part2 "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"))))
+  (testing "Puzzle data"
+    (is (= 10915059201 (part2 PUZZLE_INPUT)))))
