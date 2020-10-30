833c27add61161e2400858740282797e9715f59f
Andrew Byala <abyala@yahoo.com>
2020-10-29 23:25:39 -0500

    Day 18, rewritten with Strings

--- src/advent_2016_clojure/day18_strings.clj
+++ src/advent_2016_clojure/day18_strings.clj
@@ -1,11 +1,14 @@
 (ns advent-2016-clojure.day18_strings)
 
+(def SAFE \.)
+(def TRAP \^)
 (defn next-line [line]
-  (let [s (str \. line \.)]
-    (apply str (map #(if (= (get s %)
-                            (get s (+ 2 %)))
-                       \. \^)
-                    (range (count line))))))
+  (let [s (str SAFE line SAFE)]
+    (->> line
+         count
+         range
+         (map #(if (= (get s %) (get s (+ % 2))) SAFE TRAP))
+         (apply str))))
 
 (defn trap-sequence [line]
   (lazy-seq (cons line (trap-sequence (next-line line)))))
@@ -14,5 +17,5 @@
   (->> line
        trap-sequence
        (take num-rows)
-       (map #(count (filter (partial = \.) %)))
+       (map #(count (filter (partial = SAFE) %)))
        (apply +)))
